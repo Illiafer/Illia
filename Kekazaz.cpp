eab3e3e67c8f09b4136cb4af3a5eef8479756ae4
Illiafer <iliaempgg@gmail.com>
2025-09-15 14:01:39 +0200

    C++

---
+++ Kekazaz.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+private:
+    int year;
+    string name;
+    string city;
+
+public:
+    Person() {
+        year = 0;
+        name = "Unknown";
+        city = "Unknown";
+    }
+
+    Person(int y, string n, string c) {
+        year = y;
+        name = n;
+        city = c;
+    }
+
+    void get_info() {
+        cout << "Year: " << year << ", Name: " << name << ", City: " << city << endl;
+    }
+};
+
+int main() {
+    Person p(2009, "Illia", "Donetsk");
+    p.get_info();
+
+    return 0;
+}

5cfe41794c9d73a2cb5ce2350b3d1550d043539e
Illiafer <iliaempgg@gmail.com>
2025-09-17 14:45:56 +0200

    calculator

---
+++ Kekazaz/Kekazaz.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    float num1, num2,res;
+    cout << "Enter first  num1: ";
+    cin >> num1;
+    cout << "Enter first  num2: ";
+    cin >> num2;
+
+    char math;
+    cout << "Enter first math symbol: ";
+    cin >> math;
+
+    if (math == '+')
+        res = num1 + num2;
+    else if (math == '-')
+        res = num1 - num2;
+    else if (math == '*')
+        res = num1 * num2;
+    else if (math == '/')
+        res = num1 / num2;
+    cout << "Result: " << res << endl;
+
+
+    return 0;
+}
